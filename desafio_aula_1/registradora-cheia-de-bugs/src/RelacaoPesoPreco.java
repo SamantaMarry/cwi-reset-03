2481a2da6c4074a1a5016f8bfe6c82def1d1b1ef
Samanta Marry <samantamaarry@gmail.com>
2021-10-04 21:45:43 -0300

    Resolvendo Primeiro bug

---
+++ desafio_aula_1/registradora-cheia-de-bugs/src/RelacaoPesoPreco.java
@@ -0,0 +1,32 @@
+public class RelacaoPesoPreco {
+
+    public static double retornaPrecoProduto(String item, int qtd) {
+        double precoTotal = 0;
+
+        if ("pao".equals(item)) {
+            precoTotal = 12.75 * (qtd * 60 / 1000);
+        }
+
+        if ("torta".equals(item)) {
+            precoTotal = 96.00 * (qtd / 16);
+        }
+
+        if ("leite".equals(item)) {
+            precoTotal = 4.48 * qtd;
+        }
+
+        if ("café".equals(item)) {
+            precoTotal = 9.56 * qtd;
+        }
+
+        if ("sanduiche".equals(item)) {
+            precoTotal = 4.5 * qtd;
+        }else{
+            System.out.println("Produto não cadastrado");
+        }
+
+        return precoTotal;
+    }
+
+}
+
