84916d5db3fb93b3172988fd9a85c3575369db09
Samanta Marry <samantamaarry@gmail.com>
2021-10-09 11:41:52 -0300

    Resolução do primeiro exercício

---
+++ Aula_02/src/main/java/Filme.java
@@ -0,0 +1,27 @@
+public class Filme {
+
+    private String nome;
+    private String descrição;
+    private Integer duraçao;
+    private Integer anoDeLançamento;
+    private Double notaDeAvaliação;
+    private Diretor diretor;
+
+    public Filme(String nome, String descrição, Integer duraçao, Integer anoDeLançamento, Double notaDeAvaliaçãoDoFilme, Diretor diretor) {
+        this.nome = nome;
+        this.descrição = descrição;
+        this.duraçao = duraçao;
+        this.anoDeLançamento = anoDeLançamento;
+        this.notaDeAvaliação = notaDeAvaliaçãoDoFilme;
+        this.diretor = diretor;
+    }
+
+    public void reproduzirOFilme(){
+        System.out.println("O nome do filme: " + this.nome);
+        System.out.println("Descrição:  " + this.descrição);
+        System.out.println("Duração: " + this.duraçao);
+        System.out.println("Nome do Diretor: " + diretor.getNome());
+    }
+
+
+}
